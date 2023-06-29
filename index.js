c245ebf72f018012855b5f94dd2d7665cda9dca4
mnes18 <138093260+mnes18@users.noreply.github.com>
2023-06-29 10:57:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function login(username, password) {
+    if (username === "admin" && password === "root") {
+      alert("Login successful");
+    } else {
+      alert("Login failed");
+    }
+  }
+  
